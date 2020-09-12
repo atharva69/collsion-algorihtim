21fc4ae51db492fa6326c593006bfbf55ed6627e
atharva69 <71137953+atharva69@users.noreply.github.com>
2020-09-12 17:56:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var fixed,moving
+
+
+
+
+function setup() {
+  createCanvas(800,400);
+  fixed=createSprite(400, 200, 50, 50);
+  moving=createSprite(500, 100, 60, 60);
+  fixed.debug=true
+  moving.debug=true
+}
+
+function draw() {
+  background("green");  
+  moving.x=mouseX
+  moving.y=mouseY 
+  if(moving.x-fixed.x<moving.width/2+fixed.width/2&&
+    fixed.x-moving.x<moving.width/2+fixed.width/2&&
+    moving.y-fixed.y<moving.height/2+fixed.height/2&&
+    fixed.y-moving.y<moving.height/2+fixed.height/2) {
+    fixed.shapeColor="red"
+    moving.shapeColor="red"
+}
+ else{
+  fixed.shapeColor="blue"
+  moving.shapeColor="blue"
+
+
+ }
+  
+  
+  
+  drawSprites();
+}
